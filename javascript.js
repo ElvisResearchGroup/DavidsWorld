093a25f69aaeee57bd1c98106eb5cf9bab5a420d
Lucy French <lucyisabellefrench@gmail.com>
2015-07-30 11:04:04 +1200

    got javascript working so that we can add new expressions to the bottom div

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
 
 
 document.getElementById("loading").innerHTML = "loading bar should go here";
+
+function add(){
+	//create a new element
+	var element = document.createElement("input");
+
+	//Create Labels
+	var label = document.createElement("Label");
+	label.innerHTML = "New Label";     
+
+	//Assign different attributes to the element.
+	element.setAttribute("type", "text");
+	element.setAttribute("value", "");
+	element.setAttribute("name", "Test Name");
+	element.setAttribute("style", "width:200px");
+
+	label.setAttribute("style", "font-weight:normal");
+
+	var lineBreak = document.createElement("br");
+
+	// 'foobar' is the div id, where new fields are to be added
+	var foo = document.getElementById("foobar");
+
+	//Append the element in page (in span).
+	foo.appendChild(label);
+	foo.appendChild(element);
+	foo.appendChild(lineBreak);
+}
+

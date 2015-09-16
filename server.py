03adf693d9853c601fa915a417738946b132c166
James Greenwood-Thessman <james@greenwood.net.nz>
2015-09-17 11:29:16 +1200

    Libaries are now listed on the main page

--- server.py
+++ server.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask import render_template
 from flask import send_from_directory
 import jinja2
+from os import listdir
+from os.path import isfile, join
+
 app = Flask(__name__) 
 
-@app.route("/")
-def index():
-    return render_template('./index.html', test=123)
+LIB_DIRECTORY = './lib/'
 
-@app.route("/lib/<lib>/<file>")
+@app.route('/')
+def index():
+    libraries =  [ dir for dir in listdir(LIB_DIRECTORY) if not isfile(join(LIB_DIRECTORY, dir))]
+    return render_template('./index.html', libraries=libraries)
+
+@app.route('/lib/<lib>/<file>')
 def library(lib, file):
     return  send_from_directory('lib', '{0}/{1}'.format(lib, file))
 

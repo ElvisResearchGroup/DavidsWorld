06184709bdb6be4d058645bed03794177b468951
AngelOfMercy <ang3l.0f.m3rcy@gmail.com>
2015-10-01 11:56:04 +1300

    recommit

--- static/js/userInterface.js
+++ static/js/userInterface.js
@@ -30,6 +30,9 @@ function add(expr){
 
 }
 
+/**
+* This function deletes an expression when the user clicks the "X" button next to the expression.
+*/
 function deleteExp(divId){
 	$('#' + divId).remove();
 
@@ -43,7 +46,7 @@ function deleteExp(divId){
 }
 
 /**
- * when user clicks go button - evaluates expressions that have been added
+ * when user clicks go button - evaluates expressions that have been added and displays true or false.
  */
 
 function go(){
@@ -51,6 +54,9 @@ function go(){
 	worldstage.sendMessage('getworldforeval')
 }
 
+/**
+* 
+*/
 function setupListeners(){
 	worldstage.on('message:evalworld', function(data){
 		world = data;
@@ -157,6 +163,10 @@ function setupListeners(){
 	});
 }
 
+/**
+* This function is used to display the corresponding logic symbol in place of the text code that the user types in the expression bar. For example
+* if the user types in '\a' in the expression bar it would be replaced by the actual unicode symbol for the forall symbol.
+*/
 function replaceString(regex, string, replacement){
 	var changed = string.replace(regex, replacement);
 	while (changed != string){
@@ -181,6 +191,9 @@ function button(operator){
 	}
 }
 
+/**
+* This function returns the unicode character for the corresponding logic symbol.
+*/
 function getSymbol(operator){
 	var symbol = "";
 	switch(operator){
@@ -218,6 +231,9 @@ function getSymbol(operator){
 
 
 //sourced from: http://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery
+/**
+* This function inserts symbol at the location of the cursor when the button is pressed. 
+*/
 function insertAtCaret(element, text) {
     if (document.selection) {
         element.focus();
